287eecbb1b9f9c1c187f4999a8796cb005da2da1
Wes Balderas <balderwj@hotmail.com>
2022-01-06 03:22:53 +0000

    working on if logic

---
+++ iftest2/ipask.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+ipchk = input("Apply an IP address ") # ths line prompts the user for input
+
+# a provided string will test true
+if ipchk:
+    print("Looks like the IP address was set: " + ipchk)
+
+else:     #if data is NOT provided
+    print("You did not provide input.")
+
+
